d0e0dfc9d7bffd49e55e204ec3b6b974b9f5c9b1
Mel Peto <melanie.peto@gmail.com>
2022-06-10 14:42:44 -0400

    js functionality in header: down arrow to scroll page on mouseover due to 100vh (informs user of content below)

---
+++ script.js
@@ -0,0 +1,17 @@
+const portfolioApp = {};
+
+portfolioApp.scrollFromHeader = function() {
+    const downArrow = document.querySelector(`.fa-chevron-down`);
+    console.log(downArrow);
+    downArrow.addEventListener(`mouseover`, function (event) {
+        const aboutSection = document.querySelector(`.about`);
+        aboutSection.scrollIntoView({behavior: `smooth`, block: `center`});
+    })
+}
+
+portfolioApp.init = () => {
+    portfolioApp.scrollFromHeader();
+}
+
+portfolioApp.init();
+
