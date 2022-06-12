477a1c402a77ccf7dcf7d3bae9c14d790ec49053
Mel Peto <melanie.peto@gmail.com>
2022-06-12 19:39:36 -0400

    removed console log and other comments from script.js

--- script.js
+++ script.js
@@ -2,28 +2,15 @@ const portfolioApp = {};
 
 portfolioApp.scrollFromHeader = function() {
     const downArrow = document.querySelector(`.fa-chevron-down`);
-    console.log(downArrow);
     downArrow.addEventListener(`mouseover`, function (event) {
         const aboutSection = document.querySelector(`.about`);
         aboutSection.scrollIntoView({behavior: `smooth`, block: `center`});
     })
 }
 
-//would like to change the picture on hover, but the replacement img is a different size, so need to fix that first
-
-// portfolioApp.changeAboutPic = function() {
-//     const aboutPicElement = document.querySelector('.aboutPicture');
-//     aboutPicElement.addEventListener('mouseenter', function (event) {
-//         event.target.src="./styles/assets/fashun.jpg";
-//     })
-//     aboutPicElement.addEventListener('mouseout', function (event) {
-//         event.target.src = "./styles/assets/melaniePortraitCopy.jpg";
-//     })
-// }
-
 portfolioApp.init = () => {
     portfolioApp.scrollFromHeader();
-    // portfolioApp.changeAboutPic();
+
 }
 
 portfolioApp.init();
