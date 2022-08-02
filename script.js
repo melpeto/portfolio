26297f683759d9e2302b86422d58466872774e87
Mel Peto <melanie.peto@gmail.com>
2022-08-02 14:42:54 -0400

    changed header font, added projects 4 and 5, added papery background to About section, changed line height on About and Projects, added effect on mouseover of About image

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const portfolioApp = {};
 
-portfolioApp.scrollFromHeader = function() {
+portfolioApp.scrollFromHeader = function () {
     const downArrow = document.querySelector(`.fa-chevron-down`);
     downArrow.addEventListener(`mouseover`, function (event) {
         const aboutSection = document.querySelector(`.about`);
-        aboutSection.scrollIntoView({behavior: `smooth`, block: `center`});
+        aboutSection.scrollIntoView({ behavior: `smooth`, block: `center` });
+    })
+}
+
+portfolioApp.switchAboutPic = function () {
+    const imageDiv = document.querySelector('.aboutImg');
+    imageDiv.addEventListener('mouseover', function () {
+        document.querySelector('.aboutImg img').src = './styles/assets/fashun-resize-lg.jpg';
+    });
+    imageDiv.addEventListener('mouseout', function () {
+        document.querySelector('.aboutImg img').src = './styles/assets/melaniePortraitCopy.jpg';
     })
 }
 
 portfolioApp.init = () => {
     portfolioApp.scrollFromHeader();
-
+    portfolioApp.switchAboutPic();
 }
 
 portfolioApp.init();
